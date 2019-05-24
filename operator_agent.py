259436ce85189e0db79ce57dd40193679edbfaa0
tarcadius <t.arcadius@gmail.com>
2019-05-24 01:58:33 -0600

    LPR/NRR
    Made a few adjustments to add optional term to increase detection probability for large leaks.

--- operator_agent.py
+++ operator_agent.py
@@ -18,48 +18,8 @@ class operator_agent:
 
 #    def work_a_day (self):
 #        '''
-#        Detect leaks during operator visits
-#        '''
-#        for leak in self.state['leaks']:
-#            if leak['status'] == 'active':
-#                prob_detect = 0.00133*7/6.186   # LPR * days of the week / leaks per well in baseline
-#                detect = np.random.binomial(1, prob_detect)
-#
-#                if detect == True:
-#                    # Add these leaks to the 'tag pool'
-#                    leak['date_found'] = self.state['t'].current_date
-#                    leak['found_by_company'] = 'operator'
-#                    leak['found_by_crew'] = 1
-#                    self.state['tags'].append(leak)
-#                    
-#                    
-#        return
-
-    def work_a_day (self):
-        '''
-        Detect leaks during operator visits
-        '''      
-        
-        active_leaks = self.timeseries['active_leaks'][self.state['t'].current_timestep]
-        leak_term = ( self.init_sum_leaks / active_leaks ) * self.init_mean_leaks
-        
-        for leak in self.state['leaks']:
-            if leak['status'] == 'active':
-                prob_detect = self.parameters['LPR'] * 7 / leak_term    # LPR * days of the week / leaks per well in baseline
-                detect = np.random.binomial(1, prob_detect)
-
-                if detect == True:
-                    # Add these leaks to the 'tag pool'
-                    leak['date_found'] = self.state['t'].current_date
-                    leak['found_by_company'] = 'operator'
-                    leak['found_by_crew'] = 1
-                    self.state['tags'].append(leak)
-                        
-        return
-    
-#    def work_a_day (self):
-#        '''
-#        Detect leaks during operator visits
+#        Simple detection of leaks during operator visits. 
+#        Detection is not a function of leak-size.
 #        '''      
 #        
 #        active_leaks = self.timeseries['active_leaks'][self.state['t'].current_timestep]
@@ -78,3 +38,28 @@ class operator_agent:
 #                    self.state['tags'].append(leak)
 #                        
 #        return
+    
+    def work_a_day (self):
+        '''
+        Detect leaks during operator visits. 
+        Detection is a function of leak-size.
+        '''      
+        
+        active_leaks = self.timeseries['active_leaks'][self.state['t'].current_timestep]
+        leak_term = ( self.init_sum_leaks / active_leaks ) * self.init_mean_leaks
+        
+        for leak in self.state['leaks']:
+            if leak['status'] == 'active':
+                prob_detect = self.parameters['LPR'] * 7 / leak_term    # LPR * days of the week / leaks per well in baseline
+                prob_detect += self.parameters['max_det_op'] * (leak['rate']/self.state['max_rate'])
+                detect = np.random.binomial(1, prob_detect)
+
+                if detect == True:
+                    # Add these leaks to the 'tag pool'
+                    leak['date_found'] = self.state['t'].current_date
+                    leak['found_by_company'] = 'operator'
+                    leak['found_by_crew'] = 1
+                    self.state['tags'].append(leak)
+                        
+        return
+    
