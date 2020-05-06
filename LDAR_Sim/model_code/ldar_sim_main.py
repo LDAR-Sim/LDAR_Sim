2a61ec3efd5dcdd5169b8f6a986f0ed35c20f6a9
Tom Barchyn <tbarchyn@gmail.com>
2020-05-06 11:18:52 -0700

    Parallel processing
    Simulations are processed in parallel with a multiprocessing pool, called with a separate function.

--- LDAR_Sim/model_code/ldar_sim_main.py
+++ LDAR_Sim/model_code/ldar_sim_main.py
@@ -19,107 +19,65 @@
 #
 # ------------------------------------------------------------------------------
 
-from weather_lookup import *
-from ldar_sim import *
-from time_counter import *
 from batch_reporting import *
+from ldar_sim_run import *
 import os
 import datetime
 import time
 import gc
+import warnings
+import multiprocessing as mp
 
-# ------------------------------------------------------------------------------
-# -----------------------------Global parameters--------------------------------
-wd = "C:/Users/tarca/PycharmProjects/LDAR_Sim/Dev/test3/"
-program_list = ['P_ref', 'P_alt', 'P_alt2']  # Programs to compare; Position one should be the reference program (P_ref)
+if __name__ == '__main__':
+    # ------------------------------------------------------------------------------
+    # -----------------------------Global parameters--------------------------------
+    wd = "C:/Users/tarca/PycharmProjects/LDAR_Sim/Dev/test3/"
+    program_list = ['P_ref', 'P_alt', 'P_alt2']  # Programs to compare; Position one should be the reference program (P_ref)
+    n_processes = None  # Number of processes to use, None = all, 1 = one virtual core, and so on.
+    warnings.filterwarnings('ignore')    # Temporarily mute warnings
 
-# -----------------------------Set up programs----------------------------------
-programs = []
-for p in range(len(program_list)):
-    file = wd + 'inputs/' + program_list[p] + '.txt'
-    exec(open(file).read())
-    programs.append(eval(program_list[p]))
+    # -----------------------------Set up programs----------------------------------
+    programs = []
+    for p in range(len(program_list)):
+        file = wd + 'inputs/' + program_list[p] + '.txt'
+        exec(open(file).read())
+        programs.append(eval(program_list[p]))
 
-n_simulations = programs[0]['n_simulations']
-spin_up = programs[0]['spin_up']
-ref_program = program_list[0]
-write_data = programs[0]['write_data']
-output_directory = wd + 'outputs/'
+    n_simulations = programs[0]['n_simulations']
+    spin_up = programs[0]['spin_up']
+    ref_program = program_list[0]
+    write_data = programs[0]['write_data']
+    output_directory = wd + 'outputs/'
 
-if not os.path.exists(output_directory):
-    os.makedirs(output_directory)
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
-for i in range(n_simulations):
-    for j in range(len(programs)):
-        parameters = programs[j]
-        parameters['working_directory'] = wd
+    # Set up simulation parameter files
+    simulations = []
+    for i in range(n_simulations):
+        for j in range(len(programs)):
+            opening_message = 'Program ' + str(j + 1) + ' of ' + str(len(programs)) + '; simulation ' + \
+                                    str(i + 1) + ' of ' + str(n_simulations)
+            simulations.append ([{'i': i, 'program': programs[j], 'wd': wd, 'opening_message': opening_message}])
 
-        gc.collect()
-        print('Program ' + str(j + 1) + ' of ' + str(len(programs)) + '; simulation ' + str(i + 1) + ' of ' + str(
-            n_simulations))
+    # Perform simulations in parallel
+    with mp.Pool (processes = n_processes) as p:
+        res = p.starmap (ldar_sim_run, simulations)
 
-        parameters['simulation'] = str(i)
+    # Do batch reporting
+    if write_data:
+        # Create a data object...
+        reporting_data = BatchReporting(output_directory, programs[0]['start_year'], spin_up, ref_program)
+        if n_simulations > 1:
+            reporting_data.program_report()
+            if len(programs) > 1:
+                reporting_data.batch_report()
+                reporting_data.batch_plots()
 
-        # ------------------------------------------------------------------------------
-        # -----------------------Initialize dynamic model state-------------------------
+    # Write metadata
+    metadata = open(output_directory + '/metadata.txt', 'w')
+    metadata.write(str(programs) + '\n' +
+                   str(datetime.datetime.now()))
 
-        state = {
-            't': None,
-            'operator': None,  # operator gets assigned during initialization
-            'methods': [],  # list of methods in action
-            'sites': [],  # sites in the simulation
-            'flags': [],  # list of sites flagged for follow-up
-            'leaks': [],  # list of all current leaks
-            'tags': [],  # leaks that have been tagged for repair
-            'weather': None,  # weather gets assigned during initialization
-            'daylight': None,  # daylight hours calculated during initialization
-            'init_leaks': [],  # the initial leaks generated at timestep 1
-            'empirical_vents': [0],  # vent distribution created during initialization
-            'max_rate': None  # the largest leak in the input file
-        }
-
-        # ------------------------Initialize timeseries data----------------------------
-
-        timeseries = {
-            'datetime': [],
-            'active_leaks': [],
-            'new_leaks': [],
-            'n_tags': [],
-            'cum_repaired_leaks': [],
-            'daily_emissions_kg': []
-        }
-
-        # -----------------------------Run simulations----------------------------------
-
-        if __name__ == '__main__':
-            # Initialize objects
-
-            state['weather'] = WeatherLookup(state, parameters)
-            state['t'] = TimeCounter(parameters)
-            sim = LdarSim(state, parameters, timeseries)
-
-        # Loop through timeseries
-        while state['t'].current_date <= state['t'].end_date:
-            sim.update()
-            state['t'].next_day()
-
-        # Clean up and write files
-        sim.finalize()
-
-# Do batch reporting
-if write_data:
-    # Create a data object...
-    reporting_data = BatchReporting(output_directory, programs[0]['start_year'], spin_up, ref_program)
-    if n_simulations > 1:
-        reporting_data.program_report()
-        if len(programs) > 1:
-            reporting_data.batch_report()
-            reporting_data.batch_plots()
-
-# Write metadata
-metadata = open(output_directory + '/metadata.txt', 'w')
-metadata.write(str(programs) + '\n' +
-               str(datetime.datetime.now()))
-
-metadata.close()
+    metadata.close()
 
