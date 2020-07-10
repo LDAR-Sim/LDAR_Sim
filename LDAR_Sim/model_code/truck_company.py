65fc743c0edbed24eb8df55c3f83a9bd8d5b3c33
tarcadius <t.arcadius@gmail.com>
2020-07-09 22:25:58 -0600

    More efficient mapping
    I moved the mapping method out of each company modules and turned it into a generic function to reduce code redundancy and set the stage for different mapping options coming later.
    
    
    Former-commit-id: 4c04c73b0864f06a2a714e9d21d8c5b8c10c166d

--- LDAR_Sim/model_code/truck_company.py
+++ LDAR_Sim/model_code/truck_company.py
@@ -21,12 +21,7 @@
 
 from truck_crew import *
 from weather_lookup import *
-from generic_functions import gap_calculator
 import numpy as np
-import os
-from osgeo import gdal
-from osgeo import osr
-
 
 class truck_company:
     def __init__(self, state, parameters, config, timeseries):
@@ -34,6 +29,7 @@ class truck_company:
         Initialize a company to manage the truck crews (e.g. a contracting company).
 
         """
+        self.name = 'truck'
         self.state = state
         self.parameters = parameters
         self.config = config
@@ -57,8 +53,8 @@ class truck_company:
             site.update({'truck_missed_leaks': 0})
 
         # Initialize 2D matrices to store deployment day (DD) counts and MCBs
-        self.DD_truck_map = np.zeros((len(self.state['weather'].longitude), len(self.state['weather'].latitude)))
-        self.MCB_truck_map = np.zeros((len(self.state['weather'].longitude), len(self.state['weather'].latitude)))
+        self.DD_map = np.zeros((len(self.state['weather'].longitude), len(self.state['weather'].latitude)))
+        self.MCB_map = np.zeros((len(self.state['weather'].longitude), len(self.state['weather'].latitude)))
 
         # Initialize the individual truck crews (the agents)
         for i in range(config['n_crews']):
@@ -93,64 +89,13 @@ class truck_company:
 
         return
 
-    def make_maps(self):
-        """
-        If requested, makes maps of proportion of timesteps that are deployment days.
-        Also outputs a map of MCB (maximum condition blackout) over period of analysis.
-        """
-
-        # For each cell, sum the total number of deployment days and divide by total number of days        
-        for lon in range(len(self.state['weather'].longitude)):
-            for lat in range(len(self.state['weather'].latitude)):
-                self.DD_truck_map[lon, lat] = (self.deployment_days[lon, lat, :].sum()) / self.parameters['timesteps']
-
-        # Calculate MCB for each cell
-        for lon in range(len(self.state['weather'].longitude)):
-            for lat in range(len(self.state['weather'].latitude)):
-                self.MCB_truck_map[lon, lat] = gap_calculator(self.deployment_days[lon, lat, :])
-
-        # Set variables necessary for writing map files
-        DD_truck_output = np.swapaxes(self.DD_truck_map, axis1=0, axis2=1)
-        MCB_truck_output = np.swapaxes(self.MCB_truck_map, axis1=0, axis2=1)
-        lon, lat = self.state['weather'].longitude, self.state['weather'].latitude
-        xmin, ymin, xmax, ymax = [lon.min(), lat.min(), lon.max(), lat.max()]
-        nrows, ncols = np.shape(DD_truck_output)
-        xres = (xmax - xmin) / float(ncols)
-        yres = (ymax - ymin) / float(nrows)
-        geotransform = (xmin, xres, 0, ymax, 0, -yres)
-
-        # Set output directory
-        os.chdir(self.parameters['output_directory'])
-
-        # Export 2D proportions matrix as map
-        output_raster = gdal.GetDriverByName('GTiff').Create('DD_truck_map_' + self.parameters['simulation'] + '.tif',
-                                                             ncols, nrows, 1, gdal.GDT_Float32)
-        output_raster.SetGeoTransform(geotransform)  # Specify file coordinates
-        srs = osr.SpatialReference()  # Establish coordinate encoding
-        srs.ImportFromEPSG(4326)  # Specify WGS84 lat/long
-        output_raster.SetProjection(srs.ExportToWkt())  # Exports the coordinate system to the file
-        output_raster.GetRasterBand(1).WriteArray(DD_truck_output)  # Writes my array to the raster
-        output_raster = None
-
-        # Exprot 2D MCB matrix as map
-        output_raster = gdal.GetDriverByName('GTiff').Create('MCB_truck_map_' + self.parameters['simulation'] + '.tif',
-                                                             ncols, nrows, 1, gdal.GDT_Float32)
-        output_raster.SetGeoTransform(geotransform)  # Specify file coordinates
-        srs = osr.SpatialReference()  # Establish coordinate encoding
-        srs.ImportFromEPSG(4326)  # Specify WGS84 lat/long
-        output_raster.SetProjection(srs.ExportToWkt())  # Exports the coordinate system to the file
-        output_raster.GetRasterBand(1).WriteArray(MCB_truck_output)  # Writes my array to the raster
-        output_raster = None
-
-        return
-
     def site_reports(self):
         """
         Writes site-level deployment days (DDs) and maximum condition blackouts (MCBs) for each site.
         """
 
         for site in self.state['sites']:
-            site['truck_prop_DDs'] = self.DD_truck_map[site['lon_index'], site['lat_index']]
-            site['truck_MCB'] = self.MCB_truck_map[site['lon_index'], site['lat_index']]
+            site['truck_prop_DDs'] = self.DD_map[site['lon_index'], site['lat_index']]
+            site['truck_MCB'] = self.MCB_map[site['lon_index'], site['lat_index']]
 
         return
