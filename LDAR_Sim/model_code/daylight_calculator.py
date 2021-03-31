2caa1196182368b28fbe2e1695cef7904b50f37e
Keegan Shaw <keegan@highwoodemissions.com>
2021-03-31 11:25:08 -0600

    Update License to MIT License
    1) Swapped out GNU Public license for MIT Licence
    File.
    2) Updated license declaration in LDAR sim src files
    3) Updated license declaration in Readme.md and added
    clause for license change, where previous versions
    should use GNU Public License.
    4) Removed version from PYproj in requirements file
    which was causing install errors in Conda.
    5) Updated Dev Enviroment setup instructions
    to combine Conda Env creation and installation,
    which reduces the number of times Python is installed
    
    
    Former-commit-id: 39a0211481a1b9a6c0778da9193572de40452f42

--- LDAR_Sim/model_code/daylight_calculator.py
+++ LDAR_Sim/model_code/daylight_calculator.py
@@ -6,16 +6,16 @@
 # Copyright (C) 2018-2020  Thomas Fox, Mozhou Gao, Thomas Barchyn, Chris Hugenholtz
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
+# it under the terms of the MIT License as published
 # by the Free Software Foundation, version 3.
 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# MIT License for more details.
 
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the MIT License
+# along with this program.  If not, see <https://opensource.org/licenses/MIT>.
 #
 # ------------------------------------------------------------------------------
 
