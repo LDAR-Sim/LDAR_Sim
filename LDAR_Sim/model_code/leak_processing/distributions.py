80f611ba5d413b28443892e319a24ae316c2ebca
Keegan Shaw <keegan@highwoodemissions.com>
2021-05-05 15:58:15 -0600

    Replaced Unit Scaler with unit conversion
    1) Added leak_dist_size parameters for units
    2) utilized the unit converter sytem to convert
       variety of units and increments to g/starting
    3) updated all program files

--- LDAR_Sim/model_code/leak_processing/distributions.py
+++ LDAR_Sim/model_code/leak_processing/distributions.py
@@ -2,6 +2,7 @@
     """
 import scipy
 import numpy as np
+from leak_processing.unit_converter import gas_convert
 
 
 def fit_dist(samples=None, fit_params=None, dist_type="lognorm", floc=0,  mu=None, sigma=None):
@@ -25,13 +26,26 @@ def fit_dist(samples=None, fit_params=None, dist_type="lognorm", floc=0,  mu=Non
     return dist(*shape, loc=loc, scale=scale)
 
 
-def dist_rvs(distribution, max_size=None, gpsec_conversion=None):
+def leak_rvs(distribution, max_size=None, gpsec_conversion=None):
+    """ Generate A random Leak, convert to g/s then checkit against
+        leaks size, run until leak is smaller than max size
+
+    Args:
+        distribution (A scipy Distribution): Distribution of leak sizes
+        max_size (int, optional): Maximum Leak Size
+        gpsec_conversion (array, optional):  Conversion Units [input_metric, input_increment]
+
+    Returns:
+        [type]: [description]
+    """
+
     while True:
         leaksize = distribution.rvs()  # Get Random Value from Distribution
-        if gpsec_conversion:
-            leaksize * gpsec_conversion  # Convert to g/s
-        if leaksize < max_size:
+        if gpsec_conversion and  \
+                gpsec_conversion[0].lower() != 'gram' and \
+                gpsec_conversion[1].lower() != "second":
+            leaksize = gas_convert(leaksize, input_metric=gpsec_conversion[0],
+                                   input_increment=gpsec_conversion[1])
+        if not max_size or leaksize < max_size:
             break  # Rerun if value is larger than maximum
-        else:
-            x = 10
     return leaksize
