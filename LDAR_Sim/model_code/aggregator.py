aee763314ae349712586e5549c8539ed721f0385
tarcadius <t.arcadius@gmail.com>
2021-05-09 10:51:03 -0600

    Brief update
    Ongoing work - brief update to add equipment groups.

---
+++ LDAR_Sim/model_code/aggregator.py
@@ -0,0 +1,24 @@
+# ------------------------------------------------------------------------------
+# Program:     The LDAR Simulator (LDAR-Sim)
+# File:        LDAR-Sim main
+# Purpose:     Interface for parameterizing and running LDAR-Sim.
+#
+# Copyright (C) 2018-2020  Thomas Fox, Mozhou Gao, Thomas Barchyn, Chris Hugenholtz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License as published
+# by the Free Software Foundation, version 3.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MIT License for more details.
+
+# You should have received a copy of the MIT License
+# along with this program.  If not, see <https://opensource.org/licenses/MIT>.
+#
+# ------------------------------------------------------------------------------
+
+def equipment_aggregation():
+
+    return
