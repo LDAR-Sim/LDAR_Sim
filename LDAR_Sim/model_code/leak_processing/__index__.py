a3e032194c346a811f6b09800ffad000b3d0d9a8
Keegan Shaw <keegan@highwoodemissions.com>
2021-05-10 13:21:24 -0600

    Leak Distribution Fixes
    1) Modified code so empirical leak rates
       or leak rate distributions could be uses
    2) Added parameter to determine if empirical
       leaks will be used via sampling, fitting
       or not at all.
    3) Moved Leak Rate distribution logic into
       Ldar_sim_run so and storage into "parameters"
       (from state) to conform with existing logic
    4) Added docstrings to unit conversion and dist
       files modules.
    5) fixed issue with venting rates.
    6) Modified leak size to leak rate for consistancy.

---
+++ LDAR_Sim/model_code/leak_processing/__index__.py
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------------------------
+# Program:     The LDAR Simulator (LDAR-Sim)
+# File:        leak_processing
+# Purpose:     fit or convert units of leak rates
+#
+# Copyright (C) 2018-2021  Thomas Fox, Mozhou Gao, Thomas Barchyn, Chris Hugenholtz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License as published
+# by the Free Software Foundation, version 3.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MIT License for more details.
+
+# You should have received a copy of the MIT License
+# along with this program.  If not, see <https://opensource.org/licenses/MIT>.
+#
+# ------------------------------------------------------------------------------
