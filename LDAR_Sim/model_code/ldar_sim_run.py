2260c57969aba0d1919b5e5ce5c926c2fb2aa2da
Tom Barchyn <tbarchyn@gmail.com>
2021-05-21 16:10:20 -0700

    Deepcopy simulation dictionary
    Prevent config changes in aircraft module from affecting other simulations
    
    
    Former-commit-id: 46ba050a8103425e9ca971383555806dfe9364b4

--- LDAR_Sim/model_code/ldar_sim_run.py
+++ LDAR_Sim/model_code/ldar_sim_run.py
@@ -21,6 +21,7 @@
 import os
 import sys
 import gc
+import copy
 import numpy as np
 import pandas as pd
 
@@ -38,6 +39,7 @@ def ldar_sim_run(simulation):
     simulation = a dictionary of simulation parameters necessary to run LDAR-Sim
     """
     # i = simulation['i']
+    simulation = copy.deepcopy (simulation)
     parameters = simulation['program']
     parameters['working_directory'] = simulation['wd']
     parameters['output_directory'] = os.path.join(
