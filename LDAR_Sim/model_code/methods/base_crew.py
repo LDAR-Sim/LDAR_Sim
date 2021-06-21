3f670c80e7477ab60f42aac3b819702d45613f6a
Keegan Shaw <keegan@highwoodemissions.com>
2021-06-21 14:06:20 -0600

    Created base classes and inheriting test methmods
    1) Created base company and crew classes
    2) Created test classes for Aircraft (test)
       and for ogi (ogi_test)
    3) Updated time offsite parameter to
       be t_bw_sites, which is settable in method
       within project files.
    4) small formatting chagnes and fixes.

---
+++ LDAR_Sim/model_code/methods/base_crew.py
@@ -0,0 +1,265 @@
+# ------------------------------------------------------------------------------
+# Program:     The LDAR Simulator (LDAR-Sim)
+# File:        crew
+# Purpose:     Initialize each crew under company
+#
+# Copyright (C) 2018-2020  Thomas Fox, Mozhou Gao, Thomas Barchyn, Chris Hugenholtz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License as published
+# by the Free Software Foundation, version 3.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MIT License for more details.
+
+# You should have received a copy of the MIT License
+# along with this program.  If not, see <https://opensource.org/licenses/MIT>.
+#
+# ------------------------------------------------------------------------------
+
+from datetime import timedelta
+import numpy as np
+
+from aggregator import aggregate
+from method_functions import measured_rate
+
+
+class crew:
+    """
+    Base class crew function. Changes made here will affect any inheriting
+    classes. To us base class, import and use as argument arguement. ie.
+
+    from methods.base_crew import base_crew
+    class crew (base_crew):
+        def __init__(self, **kwargs):
+            super(crew, self).__init__(**kwargs)
+        ...s
+
+    overwrite methods by creating methods in the inheriting class
+    after the __init__ function.
+    """
+
+    def __init__(self, state, parameters, config, timeseries, deployment_days, id):
+        """
+        Constructs an individual crew based on defined configuration.
+        """
+        self.state = state
+        self.parameters = parameters
+        self.config = config
+        self.timeseries = timeseries
+        self.deployment_days = deployment_days
+        self.crewstate = {'id': id}  # Crewstate is unique to this agent
+        self.crewstate['lat'] = 0.0
+        self.crewstate['lon'] = 0.0
+        self.worked_today = False
+        self.rollover = []
+        return
+
+    def work_a_day(self, candidate_flags=None):
+        """
+        Go to work and find the leaks for a given day
+        """
+        m_name = self.config['name']
+        m_obj = self.parameters['methods'][m_name]
+        self.worked_today = False
+        self.candidate_flags = candidate_flags
+        work_hours = None
+        max_work = m_obj['max_workday']
+
+        if m_obj['consider_daylight']:
+            daylight_hours = self.state['daylight'].get_daylight(self.state['t'].current_timestep)
+            if daylight_hours <= max_work:
+                work_hours = daylight_hours
+            elif daylight_hours > max_work:
+                work_hours = max_work
+        elif not m_obj['consider_daylight']:
+            work_hours = max_work
+
+        if work_hours < 24 and work_hours != 0:
+            start_hour = (24 - work_hours) / 2
+            end_hour = start_hour + work_hours
+        else:
+            print(
+                'Unreasonable number of work hours specified for OGI crew ' +
+                str(self.crewstate['id']))
+
+        self.allowed_end_time = self.state['t'].current_date.replace(
+            hour=int(end_hour), minute=0, second=0)
+        self.state['t'].current_date = self.state['t'].current_date.replace(
+            hour=int(start_hour))  # Set start of work day
+
+        # Start day with random "t_bw_sites" required for driving to first site
+        self.state['t'].current_date += timedelta(
+            minutes=int(np.random.choice(m_obj['t_bw_sites'])))
+
+        # Check if there is a partially finished site from yesterday
+        if len(self.rollover) > 0:
+            # Check to see if the remainder of this site can be finished today
+            # (if not, this one is huge!) projection includes the time it would
+            #  time to travel back to the home base
+            projected_end_time = self.state['t'].current_date + \
+                timedelta(minutes=int(self.rollover[1]))
+            travel_home = timedelta(minutes=int(np.random.choice(m_obj['t_bw_sites'])))
+            if (projected_end_time + travel_home) > self.allowed_end_time:
+                # There's not enough time left for that site today -
+                #  get started and figure out how much time remains
+                minutes_remaining = (projected_end_time - self.allowed_end_time).total_seconds()/60
+                self.rollover = []
+                self.rollover.append(self.rollover[0])
+                self.rollover.append(minutes_remaining)
+                self.state['t'].current_date = self.allowed_end_time
+                self.worked_today = True
+            elif (projected_end_time + travel_home) <= self.allowed_end_time:
+                # Looks like we can finish off that site today
+                self.visit_site(self.rollover[0])
+                self.rollover = []
+                self.worked_today = True
+
+        while self.state['t'].current_date < self.allowed_end_time:
+            facility_ID, found_site, site = self.choose_site()
+            if not found_site:
+                break  # Break out if no site can be found
+
+            # Check to make sure there's enough time left in the day to do this site
+            # This projection includes the time it would time to travel back to the home base
+            if found_site:
+                projected_end_time = self.state['t'].current_date + \
+                    timedelta(minutes=int(site['{}_time'.format(m_name)]))
+                travel_home = timedelta(minutes=int(np.random.choice(m_obj['t_bw_sites'])))
+                if (projected_end_time + travel_home) > self.allowed_end_time:
+                    # There's not enough time left for that site today
+                    # - get started and figure out how much time remains
+                    minutes_remaining = (
+                        projected_end_time - self.allowed_end_time).total_seconds()/60
+                    self.rollover = []
+                    self.rollover.append(site)
+                    self.rollover.append(minutes_remaining)
+                    self.state['t'].current_date = self.allowed_end_time
+
+                # There's enough time left in the day for this site
+                elif (projected_end_time + travel_home) <= self.allowed_end_time:
+                    self.visit_site(site)
+                self.worked_today = True
+
+        if self.worked_today:
+            self.timeseries['{}_cost'.format(m_name)][self.state['t'].current_timestep] += \
+                m_obj['cost_per_day']
+            self.timeseries['total_daily_cost'][self.state['t'].current_timestep] += \
+                m_obj['cost_per_day']
+
+        return
+
+    def choose_site(self):
+        """
+        Choose a site to survey.
+
+        """
+        m_name = self.config['name']
+        if self.config['is_follow_up']:
+            site_pool = self.state['flags']
+        else:
+            site_pool = self.state['sites']
+        # Sort all sites based on a neglect ranking
+        site_pool = sorted(
+            site_pool,
+            key=lambda k: k['{}_t_since_last_LDAR'.format(m_name)],
+            reverse=True)
+        site = None
+        facility_ID = None  # The facility ID gets assigned if a site is found
+        found_site = False  # The found site flag is updated if a site is found
+
+        # Then, starting with the most neglected site, check if conditions are suitable for LDAR
+        for site in site_pool:
+            if not site['{}_attempted_today?'.format(m_name)]:
+                # hasn't met the minimum interval set  out in the LDAR regulations/policy),
+                # break out - no LDAR today . Sites are sorted,sub sebsequent will not meet
+                # Criteria either.
+                if self.config['is_follow_up']:
+                    is_ready = (self.state['t'].current_date - site['date_flagged']).days >= \
+                        self.parameters['methods'][site['flagged_by']]['reporting_delay']
+
+                else:
+                    if site['{}_t_since_last_LDAR'.format(m_name)] \
+                            < int(site['{}_min_int'.format(m_name)]):
+                        self.state['t'].current_date = self.state['t'].current_date.replace(hour=23)
+                        break
+                    # Else if site-specific required visits have not been met for the year
+                    elif site['{}_surveys_done_this_year'.format(m_name)] \
+                            < int(site['{}_RS'.format(m_name)]):
+                        is_ready = True
+                    else:
+                        is_ready = False
+                        # Check the weather for that site
+
+                if is_ready:
+                    site['{}_attempted_today?'.format(m_name)] = True
+                    if self.deployment_days[site['lon_index'],
+                                            site['lat_index'],
+                                            self.state['t'].current_timestep]:
+
+                        # The site passes all the tests! Choose it!
+                        facility_ID = site['facility_ID']
+                        found_site = True
+
+                        # Update site
+                        site['{}_surveys_conducted'.format(m_name)] += 1
+                        site['{}_surveys_done_this_year'.format(m_name)] += 1
+                        site['{}_t_since_last_LDAR'.format(m_name)] = 0
+                        break
+        return (facility_ID, found_site, site)
+
+    def visit_site(self, site):
+        """
+        Look for emissions at the chosen site.
+        """
+        m_name = self.config['name']
+        m_obj = self.parameters['methods'][m_name]
+
+        # Aggregate true emissions to equipment and site level; get list of leaks present
+        leaks_present, equipment_rates, site_true_rate = aggregate(site, self.state['leaks'])
+
+        # Add vented emissions
+        venting = 0
+        if self.parameters['consider_venting']:
+            venting = self.state['empirical_vents'][
+                np.random.randint(0, len(self.state['empirical_vents']))]
+        site_true_rate += venting
+        for rate in range(len(equipment_rates)):
+            equipment_rates[rate] += venting/int(site['equipment_groups'])
+
+        # Test detection module
+        site_measured_rate = 0
+        if self.config["measurement_scale"] == "site":
+            if site_true_rate > (self.config['MDL']):
+                # If source is above follow-up threshold, calculate measured rate using QE
+                site_measured_rate = measured_rate(site_true_rate, self.config['QE'])
+
+        if self.config["measurement_scale"] == "equipment":
+            for rate in equipment_rates:
+                if rate > (self.config['MDL']):
+                    equip_measured_rate = measured_rate(rate, self.config['QE'])
+                    site_measured_rate += equip_measured_rate
+
+        # If source is above follow-up threshold
+        if site_measured_rate > self.config['follow_up_thresh']:
+            # Put all necessary information in a dictionary to be assessed at end of day
+            site_dict = {
+                'site': site,
+                'leaks_present': leaks_present,
+                'site_true_rate': site_true_rate,
+                'site_measured_rate': site_measured_rate,
+                'venting': venting
+            }
+
+            self.candidate_flags.append(site_dict)
+
+        else:
+            site['{}_missed_leaks'.format(m_name)] += len(leaks_present)
+
+        self.state['t'].current_date += timedelta(minutes=int(site['{}_time'.format(m_name)]))
+        self.state['t'].current_date += timedelta(
+            minutes=int(np.random.choice(m_obj['t_bw_sites'])))
+        self.timeseries['{}_sites_visited'.format(m_name)][self.state['t'].current_timestep] += 1
+        return
---
+++ LDAR_Sim/model_code/test_OGI_company.py
@@ -0,0 +1,96 @@
+
+import math
+from datetime import timedelta
+from numpy.random import choice, normal, binomial
+
+from methods.base_company import company as bcomp
+from methods.base_crew import crew as bcrew
+
+
+class test_OGI_company(bcomp):
+    """Test Company module. Company managing fixed agents.
+
+    Args:
+        bcomp (class): Inherited base company
+    """
+
+    def __init__(self, state, parameters, config, timeseries):
+        # Run The base class Init
+        super(test_OGI_company, self).__init__(state, parameters, config, timeseries)
+        # --------- Add custom functionality Here! ----------------
+
+        # ---------------------------------------------------------
+        # Initiate Crews - This is done outside of base class
+        for i in range(config['n_crews']):
+            self.crews.append(crew(state, parameters, config,
+                                   timeseries, self.deployment_days, id=i + 1))
+
+
+class crew(bcrew):
+    """Test Company module. Initialize each crew for company.
+    OGI has a unique method of detection, so default visit_site routine
+    is overwritten.
+
+    Args:
+        bcrew (class): Inherited base crew
+    """
+
+    def __init__(self, state, parameters, config, timeseries, deployment_days, id):
+        super(crew, self).__init__(state, parameters, config, timeseries, deployment_days, id)
+        # --------- Add custom functionality Here! ----------------
+
+    def visit_site(self, site):
+        m_name = self.config['name']
+        """
+        Look for leaks at the chosen site.
+        """
+
+        # Identify all the leaks at a site
+        leaks_present = []
+        for leak in self.state['leaks']:
+            if leak['facility_ID'] == site['facility_ID']:
+                if leak['status'] == 'active':
+                    leaks_present.append(leak)
+
+        # Use an OGI camera to detect leaks
+        self.detect_leaks(site, leaks_present)
+
+        self.state['t'].current_date += timedelta(minutes=int(site['{}_time'.format(m_name)]))
+        self.state['t'].current_date += timedelta(
+            minutes=int(choice(self.config['t_bw_sites'])))
+        self.timeseries['{}_sites_visited'.format(m_name)][self.state['t'].current_timestep] += 1
+
+        if self.config['is_follow_up']:
+            # Remove site from flag pool
+            site['currently_flagged'] = False
+        return
+
+    def detect_leaks(self, site, leaks):
+        for leak in leaks:
+            k = normal(4.9, 0.3)
+            x0 = normal(self.config['MDL'][0], self.config['MDL'][1])
+            x0 = math.log10(x0 * 3600)  # Convert from g/s to g/h and take log
+
+            if leak['rate'] == 0:
+                prob_detect = 0
+            else:
+                x = math.log10(leak['rate'] * 3600)  # Convert from g/s to g/h
+                prob_detect = 1 / (1 + math.exp(-k * (x - x0)))
+            detect = binomial(1, prob_detect)
+
+            if detect:
+                if leak['tagged']:
+                    self.timeseries[self.config['name'] +
+                                    '_redund_tags'][self.state['t'].current_timestep] += 1
+
+                # Add these leaks to the 'tag pool'
+                elif not leak['tagged']:
+                    leak['tagged'] = True
+                    leak['date_tagged'] = self.state['t'].current_date
+                    leak['tagged_by_company'] = self.config['name']
+                    leak['tagged_by_crew'] = self.crewstate['id']
+                    self.state['tags'].append(leak)
+
+            elif not detect:
+                site[self.config['name'] + '_missed_leaks'] += 1
+    # ---------------------------------------------------------
