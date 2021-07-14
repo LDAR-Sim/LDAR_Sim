ae6cb975b202bb3d843cf883cf36917909f839b3
Keegan Shaw <keegan@highwoodemissions.com>
2021-07-14 11:09:32 -0600

    Added a calc number of working crews today func
    1) Added function to Mobile company to calc
       crews required to work that day. Currently
       hard coded sites per crew per day.
    2) Added docstrings to Company functions
    
    
    Former-commit-id: 729dbc871faa32f6911ea20ed80740d3fb7ed065

--- LDAR_Sim/model_code/methods/deployment/mobile_company.py
+++ LDAR_Sim/model_code/methods/deployment/mobile_company.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.cluster import KMeans
 import numpy as np
+import math
 
 
 class Schedule:
@@ -10,6 +11,10 @@ class Schedule:
         self.state = state
 
     def get_deployment_dates(self):
+        """ Using input parameters get the range of years and months available
+            for company/ crew deployment. If non are specified, set to the
+            number of years within simulation and all months.
+        """
         # if user does not specify deployment interval, set to all months/years
         if len(self.config['scheduling']['deployment_years']) > 0:
             self.deployment_years = self.config['scheduling']['deployment_years']
@@ -22,11 +27,20 @@ class Schedule:
         else:
             self.deployment_months = list(range(1, 13))
 
-    def can_deploy(self, date):
-        #  If date passed is in deployment month and year return true
+    def can_deploy_today(self, date):
+        """ If the current day is within the deployment month and years window
+        Args:
+            date (datetime): Current Date
+
+        Returns:
+            Boolean: If date passed is in deployment month and year
+        """
         return date.month in self.deployment_months and date.year in self.deployment_years
 
     def assign_agents(self):
+        """[summary]
+                ---HBD MO!!!  Describe what happens hear ---
+        """
         # Use clustering analysis to assign facilities to each agent,
         # if 2+ agents are available
         if self.config['n_crews'] > 1:
@@ -52,6 +66,24 @@ class Schedule:
             self.state['sites'][i]['label'] = label[i]
 
     def get_due_sites(self, site_pool):
+        """ Retrieve a site list of sites due for screen / survey
+
+            If the method is a followup, return sites that have passed
+            that have passed the reporting delay window.
+
+            If the method is not followup return sites that have passed
+            the minimum survey interval, and that still require surveys
+            in the current year.
+
+            # --Note-- this function persists the site list throughout,
+            so the input site_pool will always be modified regardless of
+            the variable assigned on return.
+
+        Args:
+            site_pool (dict): List of sites
+        Returns:
+            site_pool (dict): List of sites ready for survey.
+        """
         name = self.config['label']
         days_since_LDAR = '{}_t_since_last_LDAR'.format(name)
         survey_done_this_year = '{}_surveys_done_this_year'.format(name)
@@ -75,15 +107,41 @@ class Schedule:
             list(site_pool), key=lambda x: x[days_since_LDAR], reverse=True)
         return site_pool
 
-    def select_crew_sites(self, site_pool, crew_num):
-        # This function divies the site pool among all crews. Sites
-        # are passed ordered on neglected, and order is kept in the
-        # output
+    def get_working_crews(self, site_pool, n_crews, sites_per_crew=3):
+        """ Get number of working crews that day. Based on estimate
+            that a crew can do 3 sites per day.
+        Args:
+            site_pool (dict): List of sites
+            n_crews (int): Number of crews
+            sites_per_crew (int, optional): Number of sites a crew can survey in a day.
+            Defaults to 3.
+
+        Returns:
+            int: Number of crews to deploy that day.
+        """
+        n_sites = len(site_pool)
+        n_crews = math.ceil(n_sites/(n_crews*sites_per_crew))
+        # cap workuing crews at max number of crews
+        if n_crews > self.config['n_crews']:
+            n_crews = self.config['n_crews']
+        return n_crews
+
+    def get_crew_site_list(self, site_pool, crew_num, n_crews):
+        """ This function divies the site pool among all crews. Ordering
+            of sites is not changed by function.
+        Args:
+            site_pool (dict): List of sites
+            crew_num (int): Integer index of crew
+            n_crews (int): Number of crews
+
+        Returns:
+            dict: Crew site list (subset of site_pool)
+        """
         if self.config['scheduling']['geography']:
             pass
         else:
             # This offsets by the crew number and increments by the
             # number of crews, n_crews= 3 ,  site_pool = [site[0], site[3], site[6]...]
             if len(site_pool) > 0:
-                site_pool = site_pool[crew_num::self.config['n_crews']]
-        return site_pool
+                crew_site_list = site_pool[crew_num::n_crews]
+        return crew_site_list
