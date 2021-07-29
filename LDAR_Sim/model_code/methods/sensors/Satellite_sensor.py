1f15b0fd7d6db25ef74151e4ad3ea87633031f87
Mozhou Gao <mozhou@ualberta.ca>
2021-07-29 15:49:04 -0700

    Add files via upload

--- LDAR_Sim/model_code/methods/sensors/Satellite_sensor.py
+++ LDAR_Sim/model_code/methods/sensors/Satellite_sensor.py
@@ -1,3 +1,25 @@
+
+# ------------------------------------------------------------------------------
+# Program:     The LDAR Simulator (LDAR-Sim)
+# File:        methods.deployment.mobile_company
+# Purpose:     Mobile company specific deployment classes and methods (ie. Scheduling)
+#
+# Copyright (C) 2018-2020  Thomas Fox, Mozhou Gao, Thomas Barchyn, Chris Hugenholtz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License as published
+# by the Free Software Foundation, version 3.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MIT License for more details.
+
+# You should have received a copy of the MIT License
+# along with this program.  If not, see <https://opensource.org/licenses/MIT>.
+#
+# ------------------------------------------------------------------------------
+
 import numpy as np
 import random
 from generic_functions import geo_idx
