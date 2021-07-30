d528a6881d4696f2ed319c07e28842f692d8cd25
Tom Barchyn <tbarchyn@gmail.com>
2021-07-30 12:32:45 -0700

    check parameter types function for recursive parameter type checking

---
+++ LDAR_Sim/model_code/utils/check_parameter_types.py
@@ -0,0 +1,59 @@
+# ------------------------------------------------------------------------------
+# Program:     The LDAR Simulator (LDAR-Sim)
+# File:        check_parameter_types
+# Purpose:     Check input parameter types
+#
+# Copyright (C) 2018-2020  Thomas Fox, Mozhou Gao, Thomas Barchyn, Chris Hugenholtz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License as published
+# by the Free Software Foundation, version 3.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MIT License for more details.
+
+# You should have received a copy of the MIT License
+# along with this program.  If not, see <https://opensource.org/licenses/MIT>.
+#
+# ------------------------------------------------------------------------------
+
+import sys
+
+
+def check_types(default, test, omit_keys = None, fatal = False):
+    """Helper function to recursively check the type of parameter dictionaries
+    :param default: default element to test
+    :param test: test element to test
+    :param omit_keys: a list of dictionary keys to omit from further recursive testing
+    :param fatal: boolean to control whether sys.exit() is called upon a error
+    """
+    # Infer 'None' to an empty list
+    if omit_keys is None:
+        omit_keys = []
+
+    if type(default) is type(test):
+        # Proceed to test for dict or list types to recursively examine
+        if isinstance(test, dict):
+            for i in test:
+                if i not in omit_keys:
+                    if i not in default:
+                        print('Key ' + i + ' present in test parameters, but not in default parameters')
+                        if fatal:
+                            sys.exit()
+
+                    else:
+                        check_types(default[i], test[i], omit_keys = omit_keys, fatal = fatal)
+
+        elif isinstance(test, list):
+            if len(test) > 0:
+                for i in range(len(test)):
+                    check_types(default[0], test[i], omit_keys = omit_keys, fatal = fatal)
+
+    else:
+        print('Parameter type mismatch')
+        print('Default parameter: ' + str(default) + ' is ' + str(type(default)))
+        print('Test parameter: ' + str(test) + ' is ' + str(type(test)))
+        if fatal:
+            sys.exit()
