91e771a8f773711720d6772c53500f00422c9f65
Mozhou Gao <mozhou@ualberta.ca>
2021-11-24 11:39:46 -0700

    remove Gdal
    Former-commit-id: 76d7e41a26322091c629059bbafd44fb4ae1bf11

--- LDAR_Sim/src/utils/generic_functions.py
+++ LDAR_Sim/src/utils/generic_functions.py
@@ -28,13 +28,8 @@ from pathlib import Path
 
 import boto3
 import ephem
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.basemap import Basemap
 import numpy as np
 from botocore.exceptions import ClientError
-# import matplotlib.pyplot as plt
-from osgeo import gdal, osr
 from shapely.geometry import Polygon
 
 
@@ -112,122 +107,6 @@ def get_prop_rate(proportion, rates):
 
     return (rate)
 
-
-def make_maps(company, sites):
-    """
-    If requested, makes maps of proportion of timesteps that are deployment days.
-    Also outputs a map of MCB (maximum condition blackout) over period of analysis.
-    """
-
-    # For each cell, sum the total number of deployment days and divide by total number of days
-    for lon in range(len(company.state['weather'].longitude)):
-        for lat in range(len(company.state['weather'].latitude)):
-            company.DD_map[lon, lat] = (
-                company.deployment_days[lon, lat, :].sum()) / company.parameters['timesteps']
-
-    # Calculate MCB for each cell
-    for lon in range(len(company.state['weather'].longitude)):
-        for lat in range(len(company.state['weather'].latitude)):
-            company.MCB_map[lon, lat] = gap_calculator(company.deployment_days[lon, lat, :])
-
-    # Set variables necessary for writing map files
-    DD_output = np.swapaxes(company.DD_map, axis1=0, axis2=1)
-    MCB_output = np.swapaxes(company.MCB_map, axis1=0, axis2=1)
-    lon, lat = company.state['weather'].longitude, company.state['weather'].latitude
-    xmin, ymin, xmax, ymax = [lon.min(), lat.min(), lon.max(), lat.max()]
-    nrows, ncols = np.shape(DD_output)
-    xres = (xmax - xmin) / float(ncols)
-    yres = (ymax - ymin) / float(nrows)
-    geotransform = (xmin, xres, 0, ymax, 0, -yres)
-
-    # Set output directory
-    os.chdir(company.parameters['output_directory'])
-
-    # Export 2D proportions matrix as map
-    output_raster = gdal.GetDriverByName('GTiff') \
-        .Create('DD_{}_map.tif'.format(company.name), ncols, nrows, 1, gdal.GDT_Float32)
-    output_raster.SetGeoTransform(geotransform)  # Specify file coordinates
-    srs = osr.SpatialReference()  # Establish coordinate encoding
-    srs.ImportFromEPSG(4326)  # Specify WGS84 lat/long
-    output_raster.SetProjection(srs.ExportToWkt())  # Exports the coordinate system to the file
-    output_raster.GetRasterBand(1).WriteArray(DD_output)  # Writes my array to the raster
-    output_raster.FlushCache()
-    output_raster = None
-
-    # Export 2D MCB matrix as map
-    output_raster = gdal.GetDriverByName('GTiff') \
-        .Create('MCB_{}_map.tif'.format(company.name), ncols, nrows, 1, gdal.GDT_Float32)
-    output_raster.SetGeoTransform(geotransform)  # Specify file coordinates
-    srs = osr.SpatialReference()  # Establish coordinate encoding
-    srs.ImportFromEPSG(4326)  # Specify WGS84 lat/long
-    output_raster.SetProjection(srs.ExportToWkt())  # Exports the coordinate system to the file
-    output_raster.GetRasterBand(1).WriteArray(MCB_output)  # Writes my array to the raster
-    output_raster.FlushCache()
-    output_raster = None
-
-    # Disabled as Basemap is now redundant
-
-    # # Make nice map images
-    # plt.rcParams["figure.figsize"] = (10, 10)
-    # map = Basemap(epsg=3401, llcrnrlon=xmin - 1, llcrnrlat=ymin - 1, urcrnrlon=xmax + 3,
-    #               urcrnrlat=ymax + 1, resolution='i', area_thresh=10000.)
-    # map.fillcontinents(color='#e8e8e8', alpha=1, zorder=1)
-    # map.drawcountries(color='black', linewidth=2, zorder=3)
-    # map.drawstates(color='black', linewidth=1, zorder=4)
-    # map.drawparallels(
-    #     np.arange(ymin, ymax, round(abs(ymin - ymax) / 3)),
-    #     color="black", labels=[1, 0, 0, 0],
-    #     fontsize=10, linewidth=0.2, zorder=5)
-    # map.drawmeridians(
-    #     np.arange(xmin, xmax, round(abs(xmin - xmax) / 3)),
-    #     color="black", labels=[0, 0, 0, 1],
-    #     fontsize=10, linewidth=0.2, zorder=6)
-
-    # sitesx, sitesy = map(
-    #     (pd.to_numeric(sites['lon']) + 360).tolist(),
-    #     pd.to_numeric(sites['lat']).tolist())
-    # map.scatter(sitesx, sitesy, marker='o', color='black', s=1, zorder=7)
-
-    # m_lon, m_lat = np.meshgrid(lon, lat)
-    # xi, yi = map(m_lon, m_lat)
-
-    # cs = map.pcolor(xi, yi, np.squeeze(DD_output), alpha=1,
-    #                 vmin=0, vmax=1, cmap='jet_r', zorder=2)
-    # cbar = map.colorbar(cs, location='bottom', pad="5%")
-    # cbar.set_alpha(1)
-    # cbar.draw_all()
-    # cbar.set_label('Proportion of days suitable for deployment', fontsize=12)
-    # plt.savefig('DD_' + company.name + '_map' + '.png', dpi=300, verbose=False)
-    # plt.clf()
-
-    # map2 = Basemap(epsg=3401, llcrnrlon=xmin - 1, llcrnrlat=ymin - 1, urcrnrlon=xmax + 3,
-    #                urcrnrlat=ymax + 1, resolution='i', area_thresh=10000.)
-    # map2.fillcontinents(color='#e8e8e8', alpha=1, zorder=1)
-    # map2.drawcountries(color='black', linewidth=2, zorder=3)
-    # map2.drawstates(color='black', linewidth=1, zorder=4)
-    # map2.drawparallels(
-    #     np.arange(ymin, ymax, round(abs(ymin - ymax) / 3)),
-    #     color="black", labels=[1, 0, 0, 0],
-    #     fontsize=10, linewidth=0.2, zorder=5)
-    # map2.drawmeridians(
-    #     np.arange(xmin, xmax, round(abs(xmin - xmax) / 3)),
-    #     color="black", labels=[0, 0, 0, 1],
-    #     fontsize=10, linewidth=0.2, zorder=6)
-    # map2.scatter(sitesx, sitesy, marker='o', color='black', s=1, zorder=7)
-
-    # m_lon, m_lat = np.meshgrid(lon, lat)
-    # xi, yi = map(m_lon, m_lat)
-
-    # cs = map2.pcolor(xi, yi, np.squeeze(MCB_output), alpha=1,
-    #                  vmin=0, vmax=365, cmap='jet', zorder=2)
-    # cbar = map2.colorbar(cs, location='bottom', pad="5%")
-    # cbar.set_alpha(1)
-    # cbar.draw_all()
-    # cbar.set_label('Maximum blackout period (days)', fontsize=12)
-    # plt.savefig('MCB_' + company.name + '_map' + '.png', dpi=300, verbose=False)
-    # plt.clf()
-
-    return
 
 
 def check_ERA5_file(dir, programs):
