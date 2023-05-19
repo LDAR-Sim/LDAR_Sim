df1012520ae64425f81b34c9634059edf5ef0842
Thomas Galesloot <57332344+ThomasGalesloot@users.noreply.github.com>
2023-05-19 14:45:06 -0600

    Moved fixures for testing crew behavior Fixures for testing crew behavior now live in fixture file to avoid having overcrowding test file

---
+++ LDAR_Sim/testing/test_methods/crew_testing_fixutres.py
@@ -0,0 +1,102 @@
+from typing import Any
+
+import pytest
+
+
+@pytest.fixture(name="mock_config_for_crew_testing_1")
+def mock_config_for_crew_testing_1_fix() -> 'dict[str, Any]':
+    return {
+        'scheduling': {
+            'LDAR_crew_init_location': [-114.062, 51.044],
+            'route_planning': False
+        },
+        'deployment_type': 'mobile',
+        'label': 'M_test',
+        'coverage': {
+            'spatial': 1.0,
+            'temporal': 1.0,
+        },
+        'sensor': {
+            'mod_loc': None,
+            'type': 'default',
+            'MDL': [1.0],
+            'QE': 0
+        },
+        'measurement_scale': 'site'
+    }
+
+
+@pytest.fixture(name="mock_site_for_crew_testing_1")
+def mock_site_for_crew_testing_1_fix() -> 'dict[str, Any]':
+    return {
+        'equipment_groups': 1,
+        'active_leaks': [
+            {
+                'leak_ID': 1,
+                'rate': 3,
+                'equipment_group': 1
+            }
+        ]
+    }
+
+
+@pytest.fixture(name="mock_params_for_crew_testing_1")
+def mock_params_for_crew_testing_1_fix() -> 'dict[str, Any]':
+    return {
+        'emissions': {
+            'consider_venting': False
+        }
+    }
+
+
+@pytest.fixture(name="mock_config_for_crew_testing_2")
+def mock_config_for_crew_testing_2_fix() -> 'dict[str, Any]':
+    return {
+        'scheduling': {
+            'LDAR_crew_init_location': [-114.062, 51.044],
+            'route_planning': False
+        },
+        'deployment_type': 'mobile',
+        'label': 'M_test',
+        'coverage': {
+            'spatial': 1.0,
+            'temporal': 1.0,
+        },
+        'sensor': {
+            'mod_loc': None,
+            'type': 'default',
+            'MDL': [1.0],
+            'QE': 0
+        },
+        'measurement_scale': 'site'
+    }
+
+
+@pytest.fixture(name="mock_site_for_crew_testing_2")
+def mock_site_for_crew_testing_2_fix() -> 'dict[str, Any]':
+    return {
+        'equipment_groups': 1,
+        'active_leaks': [
+            {
+                'leak_ID': 1,
+                'rate': 3,
+                'equipment_group': 1
+            }
+        ]
+    }
+
+
+@pytest.fixture(name="mock_params_for_crew_testing_2")
+def mock_params_for_crew_testing_2_fix() -> 'dict[str, Any]':
+    return {
+        'emissions': {
+            'consider_venting': True
+        }
+    }
+
+
+@pytest.fixture(name="mock_state_for_crew_testing_2")
+def mock_state_for_crew_testing_2_fix() -> 'dict[str,Any]':
+    return {
+        'empirical_vents': [1]
+    }
--- LDAR_Sim/testing/test_methods/test_crew.py
+++ LDAR_Sim/testing/test_methods/test_crew.py
@@ -2,111 +2,20 @@
 
 from typing import Any
 
-import pytest
 import os
 from src.methods.crew import BaseCrew
 import numpy as np
 from pathlib import Path
 import sys
 
-
-@pytest.fixture
-def mock_config_for_crew_testing_1() -> 'dict[str, Any]':
-    return {
-        'scheduling': {
-            'LDAR_crew_init_location': [-114.062, 51.044],
-            'route_planning': False
-        },
-        'deployment_type': 'mobile',
-        'label': 'M_test',
-        'coverage': {
-            'spatial': 1.0,
-            'temporal': 1.0,
-        },
-        'sensor': {
-            'mod_loc': None,
-            'type': 'default',
-            'MDL': [1.0],
-            'QE': 0
-        },
-        'measurement_scale': 'site'
-    }
-
-
-@pytest.fixture()
-def mock_site_for_crew_testing_1() -> 'dict[str, Any]':
-    return {
-        'equipment_groups': 1,
-        'active_leaks': [
-            {
-                'leak_ID': 1,
-                'rate': 3,
-                'equipment_group': 1
-            }
-        ]
-    }
-
-
-@pytest.fixture()
-def mock_params_for_crew_testing_1() -> 'dict[str, Any]':
-    return {
-        'emissions': {
-            'consider_venting': False
-        }
-    }
-
-
-@pytest.fixture
-def mock_config_for_crew_testing_2() -> 'dict[str, Any]':
-    return {
-        'scheduling': {
-            'LDAR_crew_init_location': [-114.062, 51.044],
-            'route_planning': False
-        },
-        'deployment_type': 'mobile',
-        'label': 'M_test',
-        'coverage': {
-            'spatial': 1.0,
-            'temporal': 1.0,
-        },
-        'sensor': {
-            'mod_loc': None,
-            'type': 'default',
-            'MDL': [1.0],
-            'QE': 0
-        },
-        'measurement_scale': 'site'
-    }
-
-
-@pytest.fixture()
-def mock_site_for_crew_testing_2() -> 'dict[str, Any]':
-    return {
-        'equipment_groups': 1,
-        'active_leaks': [
-            {
-                'leak_ID': 1,
-                'rate': 3,
-                'equipment_group': 1
-            }
-        ]
-    }
-
-
-@pytest.fixture()
-def mock_params_for_crew_testing_2() -> 'dict[str, Any]':
-    return {
-        'emissions': {
-            'consider_venting': True
-        }
-    }
-
-
-@pytest.fixture()
-def mock_state_for_crew_testing_2() -> 'dict[str,Any]':
-    return {
-        'empirical_vents': [1]
-    }
+from testing.test_methods.crew_testing_fixutres import (                    # noqa: F401
+                                    mock_site_for_crew_testing_1_fix,
+                                    mock_config_for_crew_testing_1_fix,
+                                    mock_params_for_crew_testing_1_fix,
+                                    mock_config_for_crew_testing_2_fix,
+                                    mock_params_for_crew_testing_2_fix,
+                                    mock_site_for_crew_testing_2_fix,
+                                    mock_state_for_crew_testing_2_fix)
 
 
 def test_004_detect_emissions_simple(mock_config_for_crew_testing_1,
