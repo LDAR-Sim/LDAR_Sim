4c4055edccdb8bf124ad760ff281c6f737d4c39e
Thomas Galesloot <57332344+ThomasGalesloot@users.noreply.github.com>
2023-08-29 16:25:38 -0600

    107 [bug] natural n tags fails to increment (#108)
    * fix: Update natural_n_tags on nat tag overwrite
    
    Reason for change:
    Previously natural_n_tags would not be updated when a leak reached NRd
    while tagged initially by a different method. This can be misleading
    while trying to verify simulation results using the timeseries.
    
    Resolution:
    Naturally repaired leaks that were tagged by an LD method will now be
    counted in natural_n_tags.
    
    Effect(s) of change:
    Natural_n_tags is now tied to all natural tags instead of initial tags.

---
+++ LDAR_Sim/testing/unit_testing/test_utils/test_attribution/attribution_testing_fixtures.py
@@ -0,0 +1,49 @@
+"""Fixtures for testing utils.attribution methods"""
+
+import pytest
+import datetime
+
+from src.time_counter import TimeCounter
+
+
+@pytest.fixture(name="mock_leak_for_update_tag_testing_1")
+def mock_leak_for_update_tag_testing_1_fix():
+    return {
+        'tagged': True,
+        'date_tagged': datetime.datetime(2017, 1, 1, 8, 0)
+    }
+
+
+@pytest.fixture(name="mock_leak_for_update_tag_testing_2")
+def mock_leak_for_update_tag_testing_2_fix():
+    return {
+        'tagged': False
+    }
+
+
+@pytest.fixture(name="mock_company_for_update_tag_testing_1")
+def mock_company_for_update_tag_testing_1_fix():
+    return "natural"
+
+
+@pytest.fixture(name="mock_TimeCounter_for_update_tag_testing_1")
+def mock_TimeCounter_for_update_tag_testing_1_fix(mocker):
+    mock_tc = mocker.Mock(TimeCounter)
+    mock_tc.current_date = datetime.datetime(2017, 1, 1, 8, 0)
+    mock_tc.current_timestep = 1
+    return mock_tc
+
+
+@pytest.fixture(name="mock_timeseries_for_update_tag_testing_1")
+def mock_timeseries_for_update_tag_testing_1_fix():
+    return {
+        'natural_n_tags': [0, 0]
+    }
+
+
+@pytest.fixture(name="mock_site_for_update_tag_testing_1")
+def mock_site_for_update_tag_testing_1_fix():
+    return {
+        'currently_flagged': False,
+        'flagged_by': None
+    }
