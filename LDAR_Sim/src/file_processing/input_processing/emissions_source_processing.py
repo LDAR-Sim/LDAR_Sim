d1fced5af02f050a80fc3999bbb178f7b8760fc2
Thomas Galesloot <57332344+ThomasGalesloot@users.noreply.github.com>
2024-01-18 16:54:43 -0700

    Feat: Add Emission Source File Processing
    Co-authored-by: SEAJang <SEAJang@users.noreply.github.com>

--- LDAR_Sim/src/file_processing/input_processing/emissions_source_processing.py
+++ LDAR_Sim/src/file_processing/input_processing/emissions_source_processing.py
@@ -17,16 +17,144 @@ along with this program.  If not, see <https://opensource.org/licenses/MIT>.
 
 ------------------------------------------------------------------------------
 """
-
+import sys
 from typing import Any
-from pandas import DataFrame
-import scipy.stats
+from pandas import DataFrame, read_csv, Series
+from numpy.random import choice as random_sample
+from scipy import stats
 import json
+from pathlib import WindowsPath
+from dataclasses import dataclass
+from numpy import exp as exponential
+
+from LDAR_Sim.src.utils.unit_converter import gas_convert
+
+
+EMISSION_SOURCES = "sources"
+EMISSION_SOURCE_DEFAULT = "default"
+EMISSION_FILE = "emissions_file"
+EMISSION_ERS = "ERS"
+EMISSION = "emissions"
+
+INVALID_EMISSIONS_SOURCE_FILE_VALUE_ERROR = (
+    "Error: LDAR-Sim could not parse source file value: {value} from source:{source}."
+    " Encountered exception: {exception}"
+)
+
+
+# TODO add support for fitting a distribution to source data
+@dataclass
+class EmissionsSourceInfo:
+    data_use: str
+    dist_type: str
+    max_emission_rate: float
+    unit_amount: str
+    units_time: str
+    source: list[float]
+
+
+class EmissionsSource:
+    def __init__(self, source_id: str):
+        self.source_identifier = source_id
+
+    def unit_conversion(
+        for_conversion: float | list[float], unit_amount: str, unit_time: str
+    ) -> float:
+        if isinstance(for_conversion, float):
+            # TODO check gas convert defaults
+            converted_val: float = gas_convert(
+                input_quantity=for_conversion, input_metric=unit_amount, input_increment=unit_time
+            )
+            return converted_val
+        elif isinstance(for_conversion, list):
+            converted_list: list[float] = []
+            for val in for_conversion:
+                converted_val: float = gas_convert(
+                    input_quantity=val,
+                    input_metric=unit_amount,
+                    input_increment=unit_time,
+                )
+                converted_list.append(converted_val)
+            return for_conversion
+
+    def get_a_rate(self) -> float:
+        return 0.0
+
+
+class EmissionsSourceDist(EmissionsSource):
+    def __init__(
+        self,
+        source_id: str,
+        unit_amount: str,
+        unit_time: str,
+        dist_type: str,
+        dist_shape: float,
+        dist_scale: float,
+        max_emis_rate: float,
+    ):
+        super().__init__(source_id=source_id)
+        self._unit_amount: str = unit_amount
+        self._unit_time: str = unit_time
+        self._distribution: stats.rv_continuous = self.generate_distribution(
+            dist_type=dist_type, dist_shape=dist_shape, dist_scale=dist_scale
+        )
+        self._max_emis_rate = max_emis_rate
+
+    def get_a_rate(self) -> float:
+        unconverted_rate: float = self._distribution.rvs()
+        if unconverted_rate > self._max_emis_rate:
+            unconverted_rate = self._max_emis_rate
+        converted_rate: float = gas_convert(
+            input_quantity=unconverted_rate,
+            input_metric=self._unit_amount,
+            input_increment=self._unit_time,
+        )
+        return converted_rate
+
+    def generate_distribution(
+        self, dist_type: str, dist_shape: str, dist_scale: str
+    ) -> stats.rv_continuous:
+        scale: float = float(dist_scale)
+        if dist_type == "lognorm":
+            scale = exponential(scale)
+        # Ensure all shape values are captured
+        shape: float | list[float] = json.loads(dist_shape)
+        if not isinstance(shape, list):
+            shape = [shape]
+        dist_type_obj = getattr(stats, dist_type)
+        distribution = dist_type_obj(*dist_shape, 0, scale)
+        return distribution
+
+
+class EmissionsSourceSample(EmissionsSource):
+    def __init__(
+        self,
+        source_id: str,
+        unit_amount: str,
+        unit_time: str,
+        samples: list[str],
+        max_emis_rate: float,
+    ):
+        super().__init__(source_id=source_id)
+        numerical_samples = [float(sample) for sample in samples]
+        self._samples = self.unit_conversion(
+            for_conversion=numerical_samples, unit_amount=unit_amount, unit_time=unit_time
+        )
+        self._max_emis_rate = self.unit_conversion(
+            for_conversion=max_emis_rate, unit_amount=unit_amount, unit_time=unit_time
+        )
+
+    def get_a_rate(self) -> float:
+        sample = random_sample(self._samples)
+        if sample > self._max_emis_rate:
+            sample = self._max_emis_rate
+        return sample
 
 
 def read_in_emissions_sources_file(
+    inputs_path: WindowsPath,
     virtual_world: dict,
-) -> DataFrame:  # TODO: this should also allow from subtype file...
+) -> None | DataFrame:
     """Get the location for the emissions sources file and read the file into a pandas DataFrame
 
     Args
@@ -34,11 +162,45 @@ def read_in_emissions_sources_file(
     Returns
 
     """
-    virtual_world["emissions"]["emissions_file"]
-    return None
+    filename: str = virtual_world[EMISSION][EMISSION_FILE]
+    filepath: WindowsPath = inputs_path / filename
+    if filename is not None:
+        emissions_sources_file = read_csv(filepath)
+        return emissions_sources_file
+    else:
+        print(
+            (
+                "Emissions file parameter is missing for the virtual world. "
+                "Please provide a valid emissions file"
+            )
+        )
+        sys.exit()
 
 
-def process_emission_source_file(emiss_source: DataFrame) -> dict[str, Any]:
+def process_emission_sources(inputs_path: WindowsPath, virtual_world: dict) -> dict[str, Any]:
+    emissions_sources = {}
+
+    # Try to read in Emissions Sources file
+    emissions_source_file = read_in_emissions_sources_file(
+        inputs_path=inputs_path, virtual_world=virtual_world
+    )
+    # Read in the emissions sources file
+    processed_emissions_sources: dict[
+        str, list[int] | stats.rv_continuous
+    ] = process_emission_source_file(emissions_source_file)
+    emissions_sources[EMISSION_SOURCES] = processed_emissions_sources
+
+    # Extract the default emissions rates
+    emissions_default: list[float] | stats.rv_continuous = processed_emissions_sources[
+        virtual_world[EMISSION][EMISSION_ERS]
+    ]
+
+    emissions_sources[EMISSION_SOURCE_DEFAULT] = emissions_default
+
+    return emissions_sources
+
+
+def process_emission_source_file(emis_sources: DataFrame) -> dict[str, Any]:
     """Process the given emissions sources DataFrame in order to
     set up the correct emissions sources types.
 
@@ -54,46 +216,92 @@ def process_emission_source_file(emiss_source: DataFrame) -> dict[str, Any]:
     Returns:
         dict[str, (List or scipy dist obj)]: _description_
     """
+    processed_emissions_data: dict[str, EmissionsSource] = {}
+    for source_name in emis_sources.columns:
+        emis_source_info: EmissionsSourceInfo = read_in_emis_source_col(
+            source_name, emis_sources[source_name]
+        )
+        if emis_source_info.data_use == "dist":
+            emis_source: EmissionsSource = EmissionsSourceDist(
+                source_id=source_name,
+                unit_amount=emis_source_info.unit_amount,
+                unit_time=emis_source_info.units_time,
+                dist_type=emis_source_info.dist_type,
+                dist_shape=emis_source_info.source[1:],
+                dist_scale=emis_source_info.source[0],
+                max_emis_rate=emis_source_info.max_emission_rate,
+            )
+        elif emis_source_info.data_use == "sample":
+            emis_source: EmissionsSource = EmissionsSourceSample(
+                source_id=source_name,
+                unit_amount=emis_source_info.unit_amount,
+                unit_time=emis_source_info.units_time,
+                samples=emis_source_info.source,
+                max_emis_rate=emis_source_info.max_emission_rate,
+            )
+        else:
+            print(f"Error, invalid emissions source information for source {source_name}")
 
-    return None
+        processed_emissions_data[source_name] = emis_source
+
+    return processed_emissions_data
 
 
-def process_emiss_samples_as_dist(emiss_source: list[float]) -> scipy.stats.rv_continuous:
-    """
-    Process the given emission source sample List and fit into
-    a scipy distribution object
-
-    Args:
-        emiss_source (list): the emissions DataFrame
-
-    Returns:
-        scipy object
-    """
-    if emiss_source is None or not emiss_source:
-        raise ValueError("emiss_source must be a non-empty list of samples")
-
-    # Initialize a lognormal distribution
-    dist = scipy.stats.lognorm()
-
+def read_in_emis_source_col(column_name: str, emis_source_col: Series) -> EmissionsSourceInfo:
+    col_vals: list[str] = emis_source_col.str.split(",")
     try:
-        # Attempt to fit the samples to the distribution
-        param = dist.fit(emiss_source, floc=0)
+        date_use: str = col_vals[0]
     except Exception as e:
-        # Handle exceptions during fitting
-        print(f"Error fitting distribution: {e}")
-        return None
-
-    loc = param[-2]
-    scale = param[-1]
-    shape = param[:-2]
-
-    if isinstance(shape, str):
-        # If shape is a string, convert it to a Python object (int or list)
-        shape = json.loads(shape)
-
-    if not isinstance(shape, list):
-        # If the shape is not a list, convert it to a list
-        shape = [shape]
-
-    # Return a distribution object with the fitted parameters
-    return dist(*shape, loc=loc, scale=scale)
+        print(
+            INVALID_EMISSIONS_SOURCE_FILE_VALUE_ERROR.format(
+                value="Data Usage", source=column_name, exception=e
+            )
+        )
+    try:
+        dist_type: str = col_vals[1]
+    except Exception as e:
+        print(
+            INVALID_EMISSIONS_SOURCE_FILE_VALUE_ERROR.format(
+                value="Distribution Type", source=column_name, exception=e
+            )
+        )
+    try:
+        max_emis_rate: float = float(col_vals[2])
+    except Exception as e:
+        print(
+            INVALID_EMISSIONS_SOURCE_FILE_VALUE_ERROR.format(
+                value="Maximum Emission Rate", source=column_name, exception=e
+            )
+        )
+    try:
+        unit_amount: str = col_vals[3]
+    except Exception as e:
+        print(
+            INVALID_EMISSIONS_SOURCE_FILE_VALUE_ERROR.format(
+                value="Units (amount)", source=column_name, exception=e
+            )
+        )
+    try:
+        units_time: str = col_vals[4]
+    except Exception as e:
+        print(
+            INVALID_EMISSIONS_SOURCE_FILE_VALUE_ERROR.format(
+                value="Units (time)", source=column_name, exception=e
+            )
+        )
+    try:
+        source: list[str] = col_vals[5:]
+    except Exception as e:
+        print(
+            INVALID_EMISSIONS_SOURCE_FILE_VALUE_ERROR.format(
+                value="Data source values", source=column_name, exception=e
+            )
+        )
+    return EmissionsSourceInfo(
+        data_use=date_use,
+        dist_type=dist_type,
+        max_emission_rate=max_emis_rate,
+        unit_amount=unit_amount,
+        units_time=units_time,
+        source=source,
+    )
