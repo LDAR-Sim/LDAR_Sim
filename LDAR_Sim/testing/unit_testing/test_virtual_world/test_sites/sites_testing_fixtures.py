089bdcb4ac4d8de2e0dbab123405be5ff05bc595
seajang <sally-jang@hotmail.com>
2024-03-10 18:58:47 -0600

    <TEST> Update Unit test
    Reason for Change:
    Existing unit tests were not passing due to change in function calls
    
    Resolution:
    Update existing unit test to account for function call changes
    
    Effects of change:
    - None in the main code

--- LDAR_Sim/testing/unit_testing/test_virtual_world/test_sites/sites_testing_fixtures.py
+++ LDAR_Sim/testing/unit_testing/test_virtual_world/test_sites/sites_testing_fixtures.py
@@ -1,8 +1,8 @@
 import pytest
 from typing import Tuple
 
-from virtual_world.infrastructure_const import Infrastructure_Constants
-from virtual_world.sites import Site
+from src.virtual_world.infrastructure_const import Infrastructure_Constants
+from src.virtual_world.sites import Site
 from datetime import date
 
 
@@ -11,13 +11,15 @@ def mock_values_for_simple_site_construction_fix() -> Tuple[str, float, float, i
     id: str = "test"
     lat: float = 34.56
     lon: float = -44.56
+    method: list[str] = []
+    start_date: date = date(2023, 1, 1)
     equipment_groups: int = 1
     prop_params: dict = {
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_ERS: None,
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_EPR: 1,
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_ED: 1,
-        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RD: 0,
-        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RC: 100,
+        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RD: {"vals": 0},
+        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RC: {"vals": 100},
         "Method_Specific_Params": {
             Infrastructure_Constants.Sites_File_Constants.SURVEY_FREQUENCY_PLACEHOLDER: {},
             Infrastructure_Constants.Sites_File_Constants.SPATIAL_PLACEHOLDER: {},
@@ -29,7 +31,7 @@ def mock_values_for_simple_site_construction_fix() -> Tuple[str, float, float, i
         },
     }
     infra_inputs: dict = {}
-    return (id, lat, lon, equipment_groups, prop_params, infra_inputs)
+    return (id, lat, lon, start_date, equipment_groups, prop_params, infra_inputs, method)
 
 
 @pytest.fixture(name="mock_site_for_simple_generate_emissions")
@@ -42,8 +44,8 @@ def mock_site_for_simple_generate_emissions_fix() -> Tuple[Site, date, date]:
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_ERS: None,
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_EPR: 1,
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_ED: 1,
-        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RD: 0,
-        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RC: 100,
+        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RD: {"vals": 0},
+        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RC: {"vals": 100},
         "Method_Specific_Params": {
             Infrastructure_Constants.Sites_File_Constants.SURVEY_FREQUENCY_PLACEHOLDER: {},
             Infrastructure_Constants.Sites_File_Constants.SPATIAL_PLACEHOLDER: {},
@@ -74,12 +76,13 @@ def mock_site_for_simple_activate_emissions_fix() -> Tuple[Site, date, int]:
     lat: float = 34.56
     lon: float = -44.56
     equipment_groups: int = 1
+    method: list[str] = []
     prop_params: dict = {
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_ERS: None,
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_EPR: 1,
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_ED: 1,
-        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RD: 0,
-        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RC: 100,
+        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RD: {"vals": 0},
+        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RC: {"vals": 100},
         "Method_Specific_Params": {
             Infrastructure_Constants.Sites_File_Constants.SURVEY_FREQUENCY_PLACEHOLDER: {},
             Infrastructure_Constants.Sites_File_Constants.SPATIAL_PLACEHOLDER: {},
@@ -91,6 +94,8 @@ def mock_site_for_simple_activate_emissions_fix() -> Tuple[Site, date, int]:
         },
     }
     infra_inputs: dict = {}
+    simulation_start_date: date = date(*[2017, 1, 1])
+    simulation_end_date: date = date(*[2017, 1, 5])
     test_site = Site(
         id=id,
         lat=lat,
@@ -98,9 +103,9 @@ def mock_site_for_simple_activate_emissions_fix() -> Tuple[Site, date, int]:
         equipment_groups=equipment_groups,
         propagating_params=prop_params,
         infrastructure_inputs=infra_inputs,
+        methods=method,
+        start_date=simulation_start_date,
     )
-    simulation_start_date: date = date(*[2017, 1, 1])
-    simulation_end_date: date = date(*[2017, 1, 5])
     test_site.generate_emissions(
         sim_start_date=simulation_start_date, sim_end_date=simulation_end_date, sim_number=1
     )
@@ -118,8 +123,8 @@ def mock_site_for_simple_get_detectable_emissions_fix() -> Tuple[Site, str]:
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_ERS: None,
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_EPR: 1,
         Infrastructure_Constants.Sites_File_Constants.REP_EMIS_ED: 1,
-        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RD: 0,
-        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RC: 100,
+        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RD: {"vals": 0},
+        Infrastructure_Constants.Sites_File_Constants.REP_EMIS_RC: {"vals": 100},
         "Method_Specific_Params": {
             Infrastructure_Constants.Sites_File_Constants.SURVEY_FREQUENCY_PLACEHOLDER: {},
             Infrastructure_Constants.Sites_File_Constants.DEPLOYMENT_MONTHS_PLACEHOLDER: {},
@@ -130,7 +135,10 @@ def mock_site_for_simple_get_detectable_emissions_fix() -> Tuple[Site, str]:
             Infrastructure_Constants.Equipment_Group_File_Constants.SURVEY_COST_PLACEHOLDER: {},
         },
     }
+    simulation_start_date: date = date(*[2017, 1, 1])
+    simulation_end_date: date = date(*[2017, 1, 5])
     infra_inputs: dict = {}
+    method = []
     test_site = Site(
         id=id,
         lat=lat,
@@ -138,10 +146,10 @@ def mock_site_for_simple_get_detectable_emissions_fix() -> Tuple[Site, str]:
         equipment_groups=equipment_groups,
         propagating_params=prop_params,
         infrastructure_inputs=infra_inputs,
+        start_date=simulation_start_date,
+        methods=method,
     )
     test_method: str = "test"
-    simulation_start_date: date = date(*[2017, 1, 1])
-    simulation_end_date: date = date(*[2017, 1, 5])
     test_site.generate_emissions(
         sim_start_date=simulation_start_date, sim_end_date=simulation_end_date, sim_number=1
     )
--- LDAR_Sim/testing/unit_testing/test_virtual_world/test_sites/test_sites_constructor.py
+++ LDAR_Sim/testing/unit_testing/test_virtual_world/test_sites/test_sites_constructor.py
@@ -1,5 +1,5 @@
 from typing import Tuple
-from virtual_world.sites import Site
+from src.virtual_world.sites import Site
 from testing.unit_testing.test_virtual_world.test_sites.sites_testing_fixtures import (  # noqa
     mock_values_for_simple_site_construction_fix,
 )
@@ -8,13 +8,17 @@ from testing.unit_testing.test_virtual_world.test_sites.sites_testing_fixtures i
 def test_000_simple_site_is_properly_constructed(
     mock_values_for_simple_site_construction: Tuple[str, float, float, int, dict, dict]
 ):
-    id, lat, lon, equip_groups, prop_params, site_info = mock_values_for_simple_site_construction
+    id, lat, lon, start_date, equip_groups, prop_params, site_info, method = (
+        mock_values_for_simple_site_construction
+    )
     test_site = Site(
         id=id,
         lat=lat,
         long=lon,
+        start_date=start_date,
         equipment_groups=equip_groups,
         propagating_params=prop_params,
         infrastructure_inputs=site_info,
+        methods=method,
     )
     assert isinstance(test_site, Site)
