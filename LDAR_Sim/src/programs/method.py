2fbe7ff36c0d7531d8ec670d0222e84930454ae4
SEAJang <43421303+SEAJang@users.noreply.github.com>
2024-07-02 17:18:46 -0600

    <FIX>: Fix deploy crews
    Reason for change:
    Found bugs while mocking to unit test cost changes
    
    Resolution:
    Fix bug with deploy crews where the crews were not being updated
    correctly
    
    Effect(s) of change:
    - Update the code such that the deploy crews method is not changing the
    dictionary that it's iterating through until the very end
    - Fix the bug with deploy crews where we were not using the correct
    variable, and we were modifying only one crew object that should not
    have been used.

--- LDAR_Sim/src/programs/method.py
+++ LDAR_Sim/src/programs/method.py
@@ -222,6 +222,7 @@ class Method:
             crew.deployed = False
             priority_queue.put((-crew.day_time_remaining, crew.crew_id, crew))
 
+        workplans_to_add = []
         # pop the site with the longest remaining hours to assign the next crew
         # while there are crews that can work
         for survey_plan in workplan.site_survey_planners.values():
@@ -247,14 +248,14 @@ class Method:
                 # Tracking Deployment statistics
                 if site_visited:
                     deploy_stats.sites_visited += 1
-                    crew.deployed = True
-                    deploy_stats.travel_time += travel_time
-                    deploy_stats.survey_time += survey_report.time_surveyed_current_day
+                assigned_crew.deployed = True
+                deploy_stats.travel_time += travel_time
+                deploy_stats.survey_time += survey_report.time_surveyed
 
                 # If this will be last survey of the day, set remaining time
                 # to 0 and track travel home time
                 if last_site_survey:
-                    crew.day_time_remaining = 0
+                    assigned_crew.day_time_remaining = 0
                     workplan.total_travel_time += travel_time
                     deploy_stats.travel_time += travel_time
                     # TODO Make sure this gets update for other travel times as well
@@ -270,7 +271,7 @@ class Method:
                     )
             # Update the survey planner. If the survey was not finished, the update will
             # indicate that the particular site needs to be requeued with higher priority
-            workplan.add_survey_report(survey_report, survey_plan)
+            workplans_to_add.append((survey_report, survey_plan))
             if survey_report.survey_complete:
                 self._site_survey_reports.append(survey_report)
                 detection_record: DetectionRecord = DetectionRecord(
@@ -289,7 +290,10 @@ class Method:
                     if site_survey_cost == 0 and self.cost > 0:
                         site_survey_cost = self.cost
                     deploy_stats.deployment_cost += site_survey_cost
-
+        for workplan_to_add in workplans_to_add:
+            # Update the survey planner. If the survey was not finished, the update will
+            # indicate that the particular site needs to be requeued with higher priority
+            workplan.add_survey_report(*workplan_to_add)
         # If the cost type for the method is per day, calculate the deployment cost for day
         # based off the number of crews being deployed
         if self.cost_type == self.PER_DAY_COST:
