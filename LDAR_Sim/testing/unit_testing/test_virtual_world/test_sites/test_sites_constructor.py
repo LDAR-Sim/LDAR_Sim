4c4cf7af1ed2fcd9158295a7f0a93695cf5545f4
Thomas Galesloot <57332344+ThomasGalesloot@users.noreply.github.com>
2024-07-03 09:47:48 -0600

    docs: Add docstring to test_sites_constructor.py
    Reason for change:
    Missing docstring in test_sites_constructor.py
    
    Resolution:
    Added docstring to test_sites_constructor.py
    
    Effect(s) of change:
    N/A

--- LDAR_Sim/testing/unit_testing/test_virtual_world/test_sites/test_sites_constructor.py
+++ LDAR_Sim/testing/unit_testing/test_virtual_world/test_sites/test_sites_constructor.py
@@ -1,3 +1,23 @@
+"""
+------------------------------------------------------------------------------
+Program:     The LDAR Simulator (LDAR-Sim)
+File:        test_sites_constructor.py
+Purpose:    Contains unit tests for the Site class constructor
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the MIT License as published
+by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MIT License for more details.
+You should have received a copy of the MIT License
+along with this program.  If not, see <https://opensource.org/licenses/MIT>.
+
+------------------------------------------------------------------------------
+"""
+
 from typing import Tuple
 from constants.infrastructure_const import Infrastructure_Constants
 from constants.param_default_const import Common_Params as cp
