0aff5ddf1924ddeda2df9651342342ee935bb3ee
Thomas Galesloot <57332344+ThomasGalesloot@users.noreply.github.com>
2024-08-06 17:29:45 -0600

    docs: Add missing docstring
    Reason for change:
    The docstring was missing from the sensitivity_simulation_manager.py
    file.
    
    Resolution:
    This commit adds the missing docstring to the file.
    
    Effect(s) of change:
    N/A

--- LDAR_Sim/src/simulation/sensitivity_simulation_manager.py
+++ LDAR_Sim/src/simulation/sensitivity_simulation_manager.py
@@ -1,3 +1,23 @@
+# ------------------------------------------------------------------------------
+# Program:     The LDAR Simulator (LDAR-Sim)
+# File:        sensitivity_simulation_manager.py
+# Purpose:     Manages the simulation for sensitivity analysis, including state
+#
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License as published
+# by the Free Software Foundation, version 3.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MIT License for more details.
+
+# You should have received a copy of the MIT License
+# along with this program.  If not, see <https://opensource.org/licenses/MIT>.
+#
+# ------------------------------------------------------------------------------
+
 import gc
 import os
 import shutil
