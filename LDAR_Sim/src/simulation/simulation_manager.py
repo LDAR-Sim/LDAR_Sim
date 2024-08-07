6a4180ab4ca1fec2f05d86282c71ddf5ca2f1ba0
Thomas Galesloot <57332344+ThomasGalesloot@users.noreply.github.com>
2024-08-07 14:32:44 -0600

    fix: Fix error when running in debug mode
    Reason for change:
    An runtime error was preventing running in debug mode due to
    missing tuple unpacking in the simulation manager.
    
    Resolution:
    Added tuple unpacking to the simulation manager to fix the error.
    
    Effect(s) of change:
    The simulation manager will now run in debug mode without error.

--- LDAR_Sim/src/simulation/simulation_manager.py
+++ LDAR_Sim/src/simulation/simulation_manager.py
@@ -225,7 +225,7 @@ class SimulationManager:
                     simulation_number=simulation_number
                 )
                 for program in program_data:
-                    simulate(program)
+                    simulate(*program)
                 print(rm.FIN_SIM_SET.format(simulation_number=simulation_number))
             print(rm.BATCH_CLEAN.format(batch_count=batch_count))
             self.summary_stats_manager.gen_summary_outputs(batch_count != 0)
